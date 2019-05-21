5c4a821af7ab6bd26f2657510f2d70f8e408bbd5
Edina Bolkeny <bolkeny.edina@gmail.com>
2019-05-21 16:25:50 +0100

    Setup testing environment

--- spec/features/listing_bookmarks_spec.rb
+++ spec/features/listing_bookmarks_spec.rb
@@ -1,3 +1,5 @@
+require 'pg'
+
 feature 'Viewing bookmarks' do
   scenario 'index page displays title' do
     visit('/')
@@ -6,6 +8,12 @@ feature 'Viewing bookmarks' do
   end
 
   scenario 'a user can view bookmarks' do
+    connection = PG.connect(dbname: 'bookmark_manager_test')
+
+    connection.exec("INSERT INTO bookmarks VALUES (1, 'https://makers.tech/');")
+    connection.exec("INSERT INTO bookmarks  VALUES(2, 'https://en-gb.facebook.com/');")
+    connection.exec("INSERT INTO bookmarks VALUES(3, 'https://www.google.co.uk/');")
+
     visit('/bookmarks')
 
     expect(page).to have_content("https://makers.tech/")
